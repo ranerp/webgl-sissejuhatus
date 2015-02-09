2f62f22928d6fa37a2251f7806e1dc057f08846c
twtChnz <raner.piibur@gmail.com>
2015-02-09 16:59:12 +0200

    little code refactor

--- lessons/lesson06/main.js
+++ lessons/lesson06/main.js
@@ -92,16 +92,20 @@ function shadersLoaded() {
 //Tekstuuri initsialiseerimine ja laadimine
 function setupAndLoadTexture() {
 
+    //Loome uue tekstuuri ja koos sellega 1x1 pikslise pildi, mis kuvatakse senikaua, kuni tekstuur saab laetud
     APP.texture = GL.createTexture();
+    GL.bindTexture(GL.TEXTURE_2D, APP.texture);
+    GL.texImage2D(GL.TEXTURE_2D, 0, GL.RGBA, 1, 1, 0, GL.RGBA,  GL.UNSIGNED_BYTE, new Uint8Array([1, 1, 1, 1]));
+    GL.texParameterf(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.REPEAT);
+    GL.texParameterf(GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.REPEAT);
+    GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.NEAREST);
+    GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST);
+
     var image = new Image();
 
     image.onload = function() {
         GL.bindTexture(GL.TEXTURE_2D, APP.texture);
         GL.texImage2D(GL.TEXTURE_2D, 0, GL.RGB, GL.RGB,  GL.UNSIGNED_BYTE, image);
-        GL.texParameterf(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.REPEAT);
-        GL.texParameterf(GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.REPEAT);
-        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.NEAREST);
-        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST);
         GL.bindTexture(GL.TEXTURE_2D, null);
     };
     image.src = TEXTURE_PATH;
